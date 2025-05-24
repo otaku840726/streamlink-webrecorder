d6e780247cf9d28525ab018b08378a11c25ee913
scott <scott@ohya.day>
2025-05-24 19:39:19 +0800

    更新 frontend/src/components/VideoPlayer.js

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import Hls from "hls.js";
 import { Dialog, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function VideoPlayer({ url, onClose }) {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    const video = videoRef.current;
+    let hls;
+    if (url && url.endsWith(".m3u8") && Hls.isSupported()) {
+      hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.ERROR, (event, data) => {
+        if (data.fatal) hls.destroy();
+      });
+    } else if (video) {
+      video.src = url;
+    }
+    return () => { if (hls) hls.destroy(); };
+  }, [url]);
+
   return (
     <Dialog open={!!url} onClose={onClose} fullWidth maxWidth="md">
       <div style={{ position: "relative", background: "#000" }}>
         <video
-          src={url}
+          ref={videoRef}
           style={{ width: "100%", height: "100%" }}
           controls
           autoPlay
