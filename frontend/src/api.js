76e940b9c38210a0765ed613108353f2ad918802
scott <scott@ohya.day>
2025-05-24 11:05:36 +0800

    新增 frontend/src/api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const API = process.env.REACT_APP_API || "http://localhost:8800";
+
+const api = {
+  async listTasks() {
+    const r = await axios.get(`${API}/tasks`);
+    return r.data;
+  },
+  async createTask(task) {
+    const r = await axios.post(`${API}/tasks`, task);
+    return r.data;
+  },
+  async updateTask(task) {
+    const r = await axios.put(`${API}/tasks/${task.id}`, task);
+    return r.data;
+  },
+  async deleteTask(id) {
+    await axios.delete(`${API}/tasks/${id}`);
+  },
+  async listRecordings(taskId) {
+    const r = await axios.get(`${API}/tasks/${taskId}/recordings`);
+    return r.data;
+  },
+  async deleteRecording(taskId, filename) {
+    await axios.delete(`${API}/tasks/${taskId}/recordings/${filename}`);
+  },
+  async getLogs(taskId) {
+    const r = await axios.get(`${API}/tasks/${taskId}/logs`);
+    return r.data;
+  },
+};
+
+export default api;
