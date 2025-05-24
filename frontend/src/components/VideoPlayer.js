d9d9bb47f78d0daf665df57907ce22e55345f1fd
scott <scott@ohya.day>
2025-05-24 18:44:07 +0800

    更新 frontend/src/components/VideoPlayer.js

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { Dialog, DialogContent, IconButton } from "@mui/material";
+import { Dialog, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function VideoPlayer({ url, onClose }) {
   return (
-    <Dialog open={!!url} onClose={onClose} fullWidth maxWidth="sm">
-      <DialogContent>
-        <video src={url} controls style={{ width: "100%" }} />
+    <Dialog open={!!url} onClose={onClose} fullWidth maxWidth="md">
+      <div style={{ position: "relative", background: "#000" }}>
+        <video
+          src={url}
+          style={{ width: "100%", height: "100%" }}
+          controls
+          autoPlay
+        >
+          不支援此格式
+        </video>
         <IconButton
+          style={{ position: "absolute", top: 0, right: 0, color: "#fff" }}
           onClick={onClose}
-          sx={{ position: "absolute", top: 8, right: 8 }}
         >
           <CloseIcon />
         </IconButton>
-      </DialogContent>
+      </div>
     </Dialog>
   );
 }
